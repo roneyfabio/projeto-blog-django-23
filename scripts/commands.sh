0c9cc22bc7b2b2ca3b6fd7b49b42eff589609437
Roney Fábio <roneyfabio@gmail.com>
2026-06-09 09:32:45 -0300

    522

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -4,12 +4,13 @@
 set -e
 
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." &
-  sleep 0.1
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." 
+  sleep 5
 done
 
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
-python manage.py collectstatic
-python manage.py migrate
-python manage.py runserver
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
